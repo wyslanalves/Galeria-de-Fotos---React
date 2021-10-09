deb98baa51a743d1eba3c562d1e7ddf012857b67
Wyslan Alves <wyslandasilvaalves@gmail.com>
2021-10-08 23:12:44 -0300

    Projeto Galeria de Fotos

---
+++ src/components/PhotoItem/styles.ts
@@ -0,0 +1,14 @@
+import styled  from "styled-components";
+
+export const Container = styled.div`
+    background-color: #3d3f43;
+    border-radius: 10px;
+    padding: 10px;
+
+    img{
+        max-width: 50%;
+        display: block;
+        margin-bottom: 10px;
+        border-radius: 10px;
+    }
+`;
---
+++ src/services/photos.ts
@@ -0,0 +1,38 @@
+import { Photo } from "../types/Photo";
+import { storage } from '../libs/firebase';
+import { ref, listAll, getDownloadURL, uploadBytes} from 'firebase/storage';
+import { v4 as creatId } from "uuid";
+import { async } from "@firebase/util";
+
+export const getAll = async () =>{
+    let list:  Photo[] = [];
+
+    const imagesFolder = ref(storage, "images");
+    const photoList = await listAll(imagesFolder);
+
+    for(let i in photoList.items){
+
+        let photoUrl = await getDownloadURL(photoList.items[i]);
+
+        list.push({
+            name: photoList.items[i].name,
+            url: photoUrl
+        });
+    }
+
+    return list;
+}
+
+export const insert = async (file: File) => {
+    if(['image/jpeg','image/jpg','image/png'].includes(file.type)){
+        let randomName = creatId();
+        let newFile = ref(storage, `images/${randomName}`);
+
+        let upload = await uploadBytes(newFile, file);
+        let photourl = await getDownloadURL(upload.ref);
+
+        return { name: upload.ref.name, url: photourl } as Photo
+    }else{
+        return new Error('Tipo de Arquivo não permitido.');
+    }
+}
